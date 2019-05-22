90a499ada1d311c5d04404a9903f184e477f7cd2
alexkar85 <418615@mail.ru>
2019-05-22 16:40:43 +0300

    Lesson_2

--- Lesson_2/9.py
+++ Lesson_2/9.py
@@ -2,3 +2,25 @@
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
 """
+
+numbers = input("Введите натуральные числа через пробел: ")
+numbers = numbers.split()
+print(numbers)
+
+max_num = 0
+sum_max = 0
+
+
+def func_sum(num):
+    if num // 10 == 0:
+        return num
+    else:
+        return num % 10 + func_sum(num // 10)
+
+
+for i in numbers:
+    if func_sum(int(i)) > sum_max:
+        max_num = i
+        sum_max = func_sum(int(i))
+
+print(f"Натуральное число: {max_num}, сумма его чисел: {sum_max}")
