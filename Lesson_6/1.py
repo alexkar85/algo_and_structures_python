ccada3517e7e860d5d715a5085dcef0d484cc8e9
alexkar85 <418615@mail.ru>
2019-06-07 23:02:52 +0300

    1 задание к уроку 6

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,66 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+
+from pympler import asizeof
+
+N = 10000000
+
+def func1():
+    a = 0
+    f = True
+    for n in range(1, N):
+        if a + n != (n * (n + 1)) / 2:
+            f = False
+            break
+        else:
+            a = a + n
+
+    sizesum = asizeof.asizeof(N) + asizeof.asizeof(a) + asizeof.asizeof(f) + asizeof.asizeof(n) 
+    sizesum += asizeof.asizeof(range(1, N)) + asizeof.asizeof("Равенство выполняется") 
+    sizesum += asizeof.asizeof("Равенство не выполняется")
+    print(f"Алгоритм занимает {sizesum} байт")
+
+    if f:
+        return "Равенство выполняется"
+    else:
+        return "Равенство не выполняется"
+
+
+K = 10
+recsizesum = 0
+
+def func3(i):
+    global recsizesum
+    recsizesum += asizeof.asizeof(i)
+    if i == K:
+        return i
+    return i + func3(i+1)
+
+def func3start():
+    sum = func3(1)
+    s = K*(K+1)/2
+
+    sizesum = asizeof.asizeof(K) + asizeof.asizeof(sum) + asizeof.asizeof(s) + recsizesum
+    sizesum += asizeof.asizeof("Равенство выполняется") + asizeof.asizeof("Равенство не выполняется")
+    print(f"Алгоритм занимает {sizesum} байт")
+
+    if sum == s:
+        return "Равенство выполняется"
+    else:
+        return "Равенство не выполняется"
+
+func1()
+func3start()
+
+# Рабочее окружение:
+#   Python version 3.7.0
+#   Windows 64-bit
+# Анализ:
+#   Рекурсивный алгоритм более требователен к памяти, потому что
+#   при каждом вызове функции (рекурсивном вызове) все локальные
+#   переменные внутри этой функции создаются на стеке заново.
+#   Тот же самый алгоритм, но написанный через цикл не требует
+#   столько памяти, сколько требует рекурсия.
+#   По этой причине мы можем вызывать рекурсию только ограниченное
+#   количество раз.
